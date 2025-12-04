591c8fc3afe193efd6aba9515a1d9eaf0c1a8d3d
KhumboMunsaka <khumbolane11@gmail.com>
2025-12-04 15:03:13 +0200

    Hiding the stufff

--- src/services/supabase/client.js
+++ src/services/supabase/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://ookznzyorjzmryulktiw.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9va3puenlvcmp6bXJ5dWxrdGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwODM1MDAsImV4cCI6MjA3OTY1OTUwMH0.Zni_JQkrUz9jzD6np3dRkAVbAqm0KPwMvniPDkV52o8"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
