91572178e058a22d77c6cd0500f1a48fab54d028
KhumboMunsaka <khumbolane11@gmail.com>
2025-12-08 15:04:48 +0200

    Hide the item

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -8,16 +8,16 @@ import ProtectedRoute from "@components/ProtectedRoute";
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/dashboardSelect" element={<DashboardSelect />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/" element={<Dashboard />} />
-      <Route path="*" element={<PageNotFound />} /> */}
+      <Route path="*" element={<PageNotFound />} />
 
       {/* <Route path="/" element={<Login />} /> */}
-      <Route path="/dashboardSelect" element={<DashboardSelect />} />
+      {/* <Route path="/dashboardSelect" element={<DashboardSelect />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Dashboard />} /> */}
       {/* Unknown routes */}
       <Route path="*" element={<PageNotFound />} />
     </Routes>
--- src/services/supabase/client.js
+++ src/services/supabase/client.js
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ookznzyorjzmryulktiw.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9va3puenlvcmp6bXJ5dWxrdGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwODM1MDAsImV4cCI6MjA3OTY1OTUwMH0.Zni_JQkrUz9jzD6np3dRkAVbAqm0KPwMvniPDkV52o8";
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
